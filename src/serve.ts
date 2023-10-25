cd6e5a75c7ab1934d995ac27ad83e6bbda688305
Robin Kahlow <tora@warlock.ai>
2023-10-25 18:30:47 +0100

    fix incomplete file serving

--- src/serve.ts
+++ src/serve.ts
@@ -1,17 +1,31 @@
 import { Router } from "@stricjs/router";
 import { dir } from "@stricjs/utils";
-import { GameComponent } from "./gameplay/components";
+import { sleep } from "bun";
 import fs from "fs/promises";
+import { GameComponent } from "./gameplay/components";
 
 async function readReplay(path: string): Promise<GameComponent[]> {
-  const arrbuf = await Bun.file(path).arrayBuffer();
-  let buffer = Buffer.from(arrbuf);
+  const retries = 5; // sometimes the file isn't completely written yet
+  for (let i = 0; i < retries; i++) {
+    try {
+      const arrbuf = await Bun.file(path).arrayBuffer();
+      let buffer = Buffer.from(arrbuf);
 
-  if (path.endsWith(".gz")) {
-    buffer = Buffer.from(await Bun.gunzipSync(buffer));
+      if (path.endsWith(".gz")) {
+        buffer = Buffer.from(await Bun.gunzipSync(buffer));
+      }
+
+      return JSON.parse(buffer.toString("utf-8")) as GameComponent[];
+    } catch {
+      if (i >= retries) {
+        throw new Error("Max retries reached");
+      }
+
+      await sleep(200);
+    }
   }
 
-  return JSON.parse(buffer.toString("utf-8")) as GameComponent[];
+  throw new Error("Unreachable");
 }
 
 const LOG_DIR = "/mnt/e/warlock_rl_logs/logs/";
