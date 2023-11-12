5fae9b52c5d04f022242d76dc1427e8915703887
Robin Kahlow <robin@ntropy.com>
2023-11-12 13:33:43 +0000

    fix render for variable nr players

--- src/render/index.ts
+++ src/render/index.ts
@@ -389,18 +389,7 @@ function startGame(replay?: GameComponent[]) {
           container.addChild(knockbackMultiplier);
         }
 
-        if (
-          [
-            "1000",
-            "1001",
-            "1002",
-            "1003",
-            "1004",
-            "1005",
-            "1006",
-            "1007",
-          ].includes(entityId)
-        ) {
+        if (entityId in components.units) {
           const playerName = new PIXI.Text(parseInt(entityId) - 1000, {
             fontSize: 24,
           });
